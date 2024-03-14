950ef0db7b0c698e34394972e135c192c17ae1fc
danielace1 <danielsudharsan1805@gmail.com>
2024-03-14 23:30:35 +0530

    init

---
+++ tic-tac-toe-game/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div className="bg-lime-400 min-h-screen">
+      <h1>Home Page</h1>
+
+      <h2>Tic tac toe game</h2>
+    </div>
+  );
+};
+
+export default HomePage;
