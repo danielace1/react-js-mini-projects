f43fa0cf946dcbd961937fe1a11a61b5de55cb50
danielace1 <danielsudharsan1805@gmail.com>
2024-03-31 22:31:03 +0530

    feat: add player's turn logic

--- tic-tac-toe-game/src/pages/2Players.jsx
+++ tic-tac-toe-game/src/pages/2Players.jsx
@@ -199,6 +199,11 @@ const VsPlayer = () => {
     return <tbody>{[0, 1, 2].map((rowIndex) => renderRow(rowIndex))}</tbody>;
   };
 
+  const handleRestart = () => {
+    setBoard(initialBoardState);
+    setCurrentPlayer("X");
+  };
+
   return (
     <div className="bg-zinc-800 p-10 min-h-screen">
       <div className="mx-auto max-w-[480px] text-white">
@@ -227,18 +232,32 @@ const VsPlayer = () => {
           </div>
 
           <div className="flex items-center bg-zinc-700 px-5 py-2 rounded-lg border-b border-gray-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 64 64"
-              className="fill-current text-white h-5 w-5 mr-2"
-            >
-              <path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z"></path>
-            </svg>{" "}
+            {currentPlayer === "X" ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 64 64"
+                className="fill-current text-white h-5 w-5 mr-2"
+              >
+                <path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z"></path>
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 64 64"
+                className="fill-current text-white h-5 w-5 mr-2"
+              >
+                <path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z"></path>
+              </svg>
+            )}
             TURN
           </div>
 
-          <div className="bg-zinc-500 p-2 !mr-5 rounded-lg hover:cursor-pointer hover:bg-zinc-600">
+          <button
+            onClick={() => handleRestart()}
+            className="bg-zinc-500 p-2 !mr-5 rounded-lg hover:cursor-pointer hover:bg-zinc-600"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="34"
@@ -248,7 +267,7 @@ const VsPlayer = () => {
             >
               <path d="M12 20q-3.35 0-5.675-2.325T4 12q0-3.35 2.325-5.675T12 4q1.725 0 3.3.712T18 6.75V5q0-.425.288-.712T19 4q.425 0 .713.288T20 5v5q0 .425-.288.713T19 11h-5q-.425 0-.712-.288T13 10q0-.425.288-.712T14 9h3.2q-.8-1.4-2.187-2.2T12 6Q9.5 6 7.75 7.75T6 12q0 2.5 1.75 4.25T12 18q1.7 0 3.113-.862t2.187-2.313q.2-.35.563-.487t.737-.013q.4.125.575.525t-.025.75q-1.025 2-2.925 3.2T12 20"></path>
             </svg>
-          </div>
+          </button>
         </div>
 
         <table className="w-full border-separate border-spacing-5">
