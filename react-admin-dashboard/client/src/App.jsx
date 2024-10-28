6c3dad19db090664e1e57df4f8545bc69032f818
danielace1 <danielsudharsan1805@gmail.com>
2024-10-29 00:05:50 +0530

    init react-admin and json server

--- react-admin-dashboard/client/src/App.jsx
+++ react-admin-dashboard/client/src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Admin, Resource, ListGuesser } from "react-admin";
+import {}
+import simpleRestProvider from "ra-data-simple-rest";
 
-function App() {
-  const [count, setCount] = useState(0)
+const dataProvider = simpleRestProvider("http://localhost:3000/products");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="products" list={ListGuesser} />
+  </Admin>
+);
 
-export default App
+export default App;
